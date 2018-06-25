e0ef21dd4ec5e81d9f5fbb53952f1e51c2378fa4
Neil Armstrong <narmstrong@baylibre.com>
2018-06-25 12:12:54 +0200

    cmd: adc: Add range check sub-command

--- cmd/adc.c
+++ cmd/adc.c
@@ -7,6 +7,7 @@
 #include <command.h>
 #include <dm.h>
 #include <adc.h>
+#include <div64.h>
 
 static int do_adc_list(cmd_tbl_t *cmdtp, int flag, int argc,
 		       char *const argv[])
@@ -86,10 +87,51 @@ static int do_adc_single(cmd_tbl_t *cmdtp, int flag, int argc,
 	return CMD_RET_SUCCESS;
 }
 
+static int do_adc_range(cmd_tbl_t *cmdtp, int flag, int argc,
+			 char *const argv[])
+{
+	unsigned int samples = 1;
+	unsigned int min, max;
+	uint64_t data = 0;
+	int ret, i;
+
+	if (argc < 5)
+		return CMD_RET_USAGE;
+
+	min = simple_strtol(argv[3], NULL, 0);
+	max = simple_strtol(argv[4], NULL, 0);
+	if (argc > 5)
+		samples = simple_strtol(argv[5], NULL, 0);
+
+	for (i = 0 ; i < samples ; ++i) {
+		unsigned shot;
+		ret = adc_channel_single_shot(argv[1],
+					      simple_strtol(argv[2], NULL, 0),
+					      &shot);
+		if (ret) {
+			printf("Error getting single shot for device %s channel %s\n",
+			       argv[1], argv[2]);
+			return CMD_RET_FAILURE;
+		}
+
+		data += shot;
+	}
+
+	data /= samples;
+
+	if (data < min || data > max)
+		return CMD_RET_FAILURE;
+
+	printf("%u < %llu < %u\n", min, data, max);
+
+	return CMD_RET_SUCCESS;
+}
+
 static cmd_tbl_t cmd_adc_sub[] = {
 	U_BOOT_CMD_MKENT(list, 1, 1, do_adc_list, "", ""),
 	U_BOOT_CMD_MKENT(info, 2, 1, do_adc_info, "", ""),
 	U_BOOT_CMD_MKENT(single, 3, 1, do_adc_single, "", ""),
+	U_BOOT_CMD_MKENT(range, 6, 1, do_adc_range, "", ""),
 };
 
 static int do_adc(cmd_tbl_t *cmdtp, int flag, int argc,
@@ -115,6 +157,7 @@ static int do_adc(cmd_tbl_t *cmdtp, int flag, int argc,
 static char adc_help_text[] =
 	"list - list ADC devices\n"
 	"adc info <name> - Get ADC device info\n"
-	"adc single <name> <channel> - Get Single data of ADC device channel";
+	"adc single <name> <channel> - Get Single data of ADC device channel\n"
+	"adc range <name> <channel> <min> <max> [samples] - Check ADC device channel value is in a specified range";
 
-U_BOOT_CMD(adc, 4, 1, do_adc, "ADC sub-system", adc_help_text);
+U_BOOT_CMD(adc, 7, 1, do_adc, "ADC sub-system", adc_help_text);
