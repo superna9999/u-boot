ecc03fb0e5eec886c6301a93136333f474817426
Neil Armstrong <narmstrong@baylibre.com>
2018-06-27 13:50:38 +0200

    boards: Amlogic: Add support for Libretech-AC

---
+++ include/configs/libretech-ac.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for LibreTech AC
+ *
+ * Copyright (C) 2017 Baylibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_MISC_INIT_R
+
+#define MESON_FDTFILE_SETTING "fdtfile=amlogic/meson-gxl-s805x-libretech-ac.dtb\0"
+
+#include <configs/meson-gx-common.h>
+
+#endif /* __CONFIG_H */
