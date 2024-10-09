067f991d7c6123fb3dfed4d593b6c3ad6881550d
Neil Armstrong <neil.armstrong@linaro.org>
2024-10-09 11:49:39 +0200

    Fixups
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/video/khadas-ts050.c
+++ drivers/video/khadas-ts050.c
@@ -4,7 +4,6 @@
  * Copyright (C) 2020 BayLibre, SAS
  * Author: Neil Armstrong <narmstrong@baylibre.com>
  */
-#include <common.h>
 #include <backlight.h>
 #include <dm.h>
 #include <mipi_dsi.h>
--- drivers/video/meson/meson_dw_mipi_dsi.c
+++ drivers/video/meson/meson_dw_mipi_dsi.c
@@ -4,7 +4,6 @@
  * Author: Neil Armstrong <narmstrong@baylibre.com>
  */
 
-#include <common.h>
 #include <display.h>
 #include <dm.h>
 #include <edid.h>
